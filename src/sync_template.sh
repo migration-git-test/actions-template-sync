b9c042b46c9f73fb691fb5dca791726d655f422f
NAVAKEERTHANA <78489664+NAVAKEERTHANA@users.noreply.github.com>
2024-12-25 19:09:17 +0530

    Update sync_template.sh
    Signed-off-by: NAVAKEERTHANA <78489664+NAVAKEERTHANA@users.noreply.github.com>

--- src/sync_template.sh
+++ src/sync_template.sh
@@ -57,6 +57,7 @@ TEMPLATE_REMOTE_GIT_HASH=$(git ls-remote "${SOURCE_REPO}" HEAD | awk '{print $1}
 SHORT_TEMPLATE_GIT_HASH=$(git rev-parse --short "${TEMPLATE_REMOTE_GIT_HASH}")
 
 export TEMPLATE_GIT_HASH=${SHORT_TEMPLATE_GIT_HASH}
+export PR_BRANCH="${PR_BRANCH_NAME_PREFIX}_${TEMPLATE_GIT_HASH}"
 : "${PR_BODY:="Merge ${SOURCE_REPO} ${TEMPLATE_GIT_HASH}"}"
 : "${PR_TITLE:-"upstream merge template repository"}"
 
@@ -73,7 +74,7 @@ PR_TITLE=${PR_TITLE//'${TEMPLATE_GIT_HASH}'/"${TEMPLATE_GIT_HASH}"}
 PR_TITLE=${PR_TITLE//'${SOURCE_REPO}'/"${SOURCE_REPO}"}
 
 debug "TEMPLATE_GIT_HASH ${TEMPLATE_GIT_HASH}"
-debug "PR_TITLE ${PR_TITLE}"
+debug "PR_BRANCH ${PR_BRANCH}"
 debug "PR_BODY ${PR_BODY}"
 
 # Check if the Ignore File exists inside .github folder or if it doesn't exist at all
@@ -86,33 +87,111 @@ fi
 # Functions
 #####################################################
 
-# Skip the branch creation and PR directly from the source repository
-function create_pr_from_source_repo() {
-  info "create pr from source repo"
-  
-  gh pr create \
-    --title "${PR_TITLE}" \
-    --body "${PR_BODY}" \
-    --base "${UPSTREAM_BRANCH}" \
-    --head "${SOURCE_REPO}:${UPSTREAM_BRANCH}" \
-    --label "${PR_LABELS}" \
-    --reviewer "${REVIEWERS}" || create_pr_has_issues=true
+#######################################
+# set the gh action outputs if run with github action.
+# Arguments:
+#   pr_branch
+#   template_git_hash
+#######################################
+function set_github_action_outputs() {
+  echo "::group::set gh action outputs"
 
-  if [ "$create_pr_has_issues" == true ] ; then
-    warn "Creating the PR failed."
-    warn "Eventually it is already existent."
-    return 1
+  local pr_branch=$1
+  local template_git_hash=$2
+
+  info "set github action outputs"
+
+  if [[ -z "${GITHUB_RUN_ID}" ]]; then
+    info "env var 'GITHUB_RUN_ID' is empty -> no github action workflow"
+  else
+    # https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+    echo "pr_branch=${pr_branch}" >> "$GITHUB_OUTPUT"
+    echo "template_git_hash=${template_git_hash}" >> "$GITHUB_OUTPUT"
   fi
-  return 0
+  echo "::endgroup::"
 }
 
-# cleanup older prs based on labels
+#######################################
+# Check if the branch exists remote.
+# Arguments:
+#   pr_branch
+#######################################
+function check_branch_remote_existing() {
+
+  local branch_to_check=$1
+
+  info "check if the remote branch ${branch_to_check} exists. Exit if so"
+
+  git ls-remote --exit-code --heads origin "${branch_to_check}" || branch_does_not_exist=true
+
+  if [[ "${branch_does_not_exist}" != true ]]; then
+    warn "Git branch '${branch_to_check}' exists in the remote repository"
+    set_github_action_outputs "${branch_to_check}"
+    exit 0
+  fi
+}
+
+#######################################
+# Check if the commit is already in history.
+# exit 0 if so
+# Arguments:
+#   template_remote_git_hash
+#######################################
+function check_if_commit_already_in_hist_graceful_exit() {
+  info "check if commit already in history"
+
+  local template_remote_git_hash=$1
+
+  git cat-file -e "${template_remote_git_hash}" || commit_not_in_hist=true
+  if [ "${commit_not_in_hist}" != true ] ; then
+    warn "repository is up to date!"
+    exit 0
+  fi
+}
+
+##########################################
+# check if there are staged files.
+# exit if not
+##########################################
+function check_staged_files_available_graceful_exit() {
+  if git diff --quiet && git diff --staged --quiet; then
+    info "nothing to commit"
+    exit 0
+  fi
+}
+
+#######################################
+# force source file deletion if they had been deleted
+#######################################
+function force_delete_files() {
+  info "force delete files"
+  warn "force file deletion is enabled. Deleting files which are deleted within the target repository"
+  local_current_git_hash=$(git rev-parse HEAD)
+
+  info "current git hash: ${local_current_git_hash}"
+
+  files_to_delete=$(git log --diff-filter D --pretty="format:" --name-only "${local_current_git_hash}"..HEAD | sed '/^$/d')
+  warn "files to delete: ${files_to_delete}"
+  if [[ -n "${files_to_delete}" ]]; then
+    echo "${files_to_delete}" | xargs rm
+  fi
+}
+
+#######################################
+# cleanup older prs based on labels.
+# Arguments:
+#   upstream_branch
+#   pr_labels
+#   is_keep_branch_on_pr_cleanup
+#   local_branch_name
+#######################################
 function cleanup_older_prs () {
   info "cleanup older prs"
 
   local upstream_branch=$1
   local pr_labels=$2
   local is_keep_branch_on_pr_cleanup=$3
+  local local_branch_name=$4
 
   if [[ -z "${pr_labels}" ]]; then
     warn "env var 'PR_LABELS' is empty. Skipping older prs cleanup"
@@ -131,8 +210,8 @@ function cleanup_older_prs () {
     branch_name=$(echo "$older_pr" | jq -r .headRefName)
     pr_number=$(echo "$older_pr" | jq -r .number)
 
-    if [ "$branch_name" == "${UPSTREAM_BRANCH}" ] ; then
-      warn "local branch name equals remote pr branch name ${UPSTREAM_BRANCH}. Skipping pr cleanup for this branch"
+    if [ "$branch_name" == "$local_branch_name" ] ; then
+      warn "local branch name equals remote pr branch name ${local_branch_name}. Skipping pr cleanup for this branch"
       continue
     fi
 
@@ -146,6 +225,182 @@ function cleanup_older_prs () {
   done
 }
 
+##################################
+# pull source changes
+# Arguments:
+#   source_repo
+#   git_remote_pull_params
+##################################
+function pull_source_changes() {
+  info "pull changes from source repository"
+  local source_repo=$1
+  local git_remote_pull_params=$2
+
+  eval "git pull ${source_repo} --tags ${git_remote_pull_params}" || pull_has_issues=true
+
+  if [ "$pull_has_issues" == true ] ; then
+    warn "There had been some git pull issues."
+    warn "Maybe a merge issue."
+    warn "We go on but it is likely that you need to fix merge issues within the created PR."
+  fi
+}
+
+#######################################
+# eventual create labels (if they are not existent).
+# Arguments:
+#   pr_labels
+#######################################
+function eventual_create_labels () {
+  local pr_labels=$1
+  info "eventual create labels ${pr_labels}"
+
+  if [[ -z "${pr_labels}" ]]; then
+    info "'pr_labels' is empty. Skipping labels check"
+    retun 0
+  fi
+
+  readarray -t labels_array < <(awk -F',' '{ for( i=1; i<=NF; i++ ) print $i }' <<<"${pr_labels}")
+  for label in "${labels_array[@]}"
+  do
+    search_result=$(gh label list \
+    --search "${label}" \
+    --limit 1 \
+    --json name \
+    --template '{{range .}}{{printf "%v" .name}}{{"\n"}}{{end}}')
+
+    if [ "${search_result}" = "${label##[[:space:]]}" ]; then
+      info "label '${label##[[:space:]]}' was found in the repository"
+    else
+      if gh label create "${label}"; then
+        info "label '${label}' was missing and has been created"
+      else
+        warn "label creation did not work. For any reason the former check sometimes is failing"
+      fi
+    fi
+  done
+}
+
+##############################
+# push the changes
+# Arguments:
+#   branch
+#   is_force
+#   is_with_tags
+##############################
+function push () {
+  info "push changes"
+  local branch=$1
+  local is_force=$2
+  local is_with_tags=$3
+
+  args=(--set-upstream origin "${branch}")
+
+  if [ "$is_force" == true ] ; then
+    warn "forcing the push."
+    args+=(--force)
+  fi
+
+  if [ "$is_with_tags" == true ] ; then
+    warn "include tags."
+    args+=(--tags)
+  fi
+
+  git push "${args[@]}"
+}
+
+####################################
+# creates a pr
+# Arguments:
+#   title
+#   body
+#   branch
+#   labels
+#   reviewers
+###################################
+function create_pr() {
+  info "create pr"
+  local title=$1
+  local body=$2
+  local branch=$3
+  local labels=$4
+  local reviewers=$5
+
+  gh pr create \
+    --title "${title}" \
+    --body "${body}" \
+    --base "${branch}" \
+    --label "${labels}" \
+    --reviewer "${reviewers}" || create_pr_has_issues=true
+
+  if [ "$create_pr_has_issues" == true ] ; then
+    warn "Creating the PR failed."
+    warn "Eventually it is already existent."
+    return 1
+  fi
+  return 0
+}
+
+####################################
+# creates or edits a pr if already existent
+# Arguments:
+#   title
+#   body
+#   upstream_branch
+#   labels
+#   reviewers
+###################################
+function create_or_edit_pr() {
+  info "create pr or edit the pr"
+  local title=$1
+  local body=$2
+  local upstream_branch=$3
+  local labels=$4
+  local reviewers=$5
+  local pr_branch=$6
+
+  create_pr "${title}" "${body}" "${upstream_branch}" "${labels}" "${reviewers}" || gh pr edit \
+    --title "${title}" \
+    --body "${body}" \
+    --add-label "${labels}" \
+    --add-reviewer "${reviewers}"
+}
+
+#########################################
+# restore the .templatesyncignore file
+# Arguments:
+#   template_sync_ignore_file_path
+###########################################
+function restore_templatesyncignore_file() {
+  info "restore the ignore file"
+  local template_sync_ignore_file_path=$1
+  if [ -s "${template_sync_ignore_file_path}" ]; then
+    git reset "${template_sync_ignore_file_path}"
+    git checkout -- "${template_sync_ignore_file_path}" || warn "not able to checkout the former .templatesyncignore file. Most likely the file was not present"
+  fi
+}
+
+#########################################
+# reset all files within the .templatesyncignore file
+# Arguments:
+#   template_sync_ignore_file_path
+###########################################
+function handle_templatesyncignore() {
+  info "handle .templatesyncignore"
+  local template_sync_ignore_file_path=$1
+  # we are checking the ignore file if it exists or is empty
+  # -s is true if the file contains whitespaces
+  if [ -s "${template_sync_ignore_file_path}" ]; then
+    debug "unstage files from template sync ignore ${template_sync_ignore_file_path}"
+    sed '/^[[:blank:]]*#/d;s/#.*//' "${template_sync_ignore_file_path}" | awk NF | xargs -r git reset --
+
+    debug "clean untracked files"
+    git clean -df
+
+    debug "discard all unstaged changes"
+    git checkout -- .
+  fi
+}
+
 ########################################################
 # Logic
 #######################################################
@@ -157,10 +412,152 @@ function arr_prechecks() {
     warn "skipping prechecks because we force push and pr"
     return 0
   fi
+  check_branch_remote_existing "${PR_BRANCH}"
+
+  check_if_commit_already_in_hist_graceful_exit "${TEMPLATE_REMOTE_GIT_HASH}"
+
   echo "::endgroup::"
 }
 
-# Main logic to create PR directly from source repo
-arr_prechecks
-create_pr_from_source_repo
-cleanup_older_prs "${UPSTREAM_BRANCH}" "${PR_LABELS}" "${IS_KEEP_BRANCH_ON_PR_CLEANUP}"
+
+function arr_checkout_branch_and_pull() {
+  info "checkout branch and pull"
+  cmd_from_yml "prepull"
+
+  echo "::group::checkout branch and pull"
+
+  debug "create new branch from default branch with name ${PR_BRANCH}"
+  git checkout -b "${PR_BRANCH}"
+  debug "pull changes from template"
+
+  pull_source_changes "${SOURCE_REPO}" "${GIT_REMOTE_PULL_PARAMS}"
+
+  restore_templatesyncignore_file "${TEMPLATE_SYNC_IGNORE_FILE_PATH}"
+
+  if [ "$IS_FORCE_DELETION" == "true" ]; then
+    force_delete_files
+  fi
+
+  echo "::endgroup::"
+}
+
+
+function arr_commit() {
+  info "commit"
+
+  cmd_from_yml "precommit"
+
+  echo "::group::commit changes"
+
+  git add .
+
+  handle_templatesyncignore "${TEMPLATE_SYNC_IGNORE_FILE_PATH}"
+
+  check_staged_files_available_graceful_exit
+
+  git commit --signoff -m "${PR_COMMIT_MSG}"
+
+  echo "::endgroup::"
+}
+
+
+function arr_push() {
+  info "push"
+
+  echo "::group::push"
+  if [ "$IS_DRY_RUN" == "true" ]; then
+    warn "dry_run option is set to on. skipping push"
+    return 0
+  fi
+  cmd_from_yml "prepush"
+  push "${PR_BRANCH}" "${IS_FORCE_PUSH_PR}" "${IS_WITH_TAGS}"
+  echo "::endgroup::"
+}
+
+function arr_prepare_pr_create_pr() {
+  info "prepare_pr_create_pr"
+  if [ "$IS_DRY_RUN" == "true" ]; then
+    warn "dry_run option is set to on. skipping labels check, cleanup older PRs, push and create pr"
+    return 0
+  fi
+  echo "::group::check for missing labels"
+
+  eventual_create_labels "${PR_LABELS}"
+
+  echo "::endgroup::"
+
+  echo "::group::cleanup older PRs"
+  if [ "$IS_PR_CLEANUP" != "false" ]; then
+    if [[ -z "${PR_LABELS}" ]]; then
+    warn "env var 'PR_LABELS' is empty. Skipping older prs cleanup"
+    else
+      cmd_from_yml "precleanup"
+      cleanup_older_prs "${UPSTREAM_BRANCH}" "${PR_LABELS}" "${IS_KEEP_BRANCH_ON_PR_CLEANUP}" "${PR_BRANCH}"
+    fi
+  else
+    warn "is_pr_cleanup option is set to off. Skipping older prs cleanup"
+  fi
+
+  echo "::endgroup::"
+
+  echo "::group::create PR"
+
+  cmd_from_yml "prepr"
+  if [ "$IS_FORCE_PUSH_PR" == true ] ; then
+    create_or_edit_pr "${PR_TITLE}" "${PR_BODY}" "${UPSTREAM_BRANCH}" "${PR_LABELS}" "${PR_REVIEWERS}"
+  else
+    create_pr "${PR_TITLE}" "${PR_BODY}" "${UPSTREAM_BRANCH}" "${PR_LABELS}" "${PR_REVIEWERS}"
+  fi
+
+
+  echo "::endgroup::"
+}
+
+declare -A cmd_arr
+declare -a orders;
+
+cmd_arr["prechecks"]=arr_prechecks; orders+=("prechecks")
+cmd_arr["pull"]=arr_checkout_branch_and_pull; orders+=("pull")
+cmd_arr["commit"]=arr_commit; orders+=("commit")
+cmd_arr["push"]=arr_push; orders+=("push")
+cmd_arr["pr"]=arr_prepare_pr_create_pr; orders+=("pr")
+
+if [[ -z "${STEPS}" ]]; then
+  info "no steps provided. Default is to execute all."
+  for key in "${orders[@]}";
+  do
+    debug "execute cmd ${key}"
+    ${cmd_arr[${key}]}
+  done
+else
+  info "steps provided."
+  readarray -t steps < <(awk -F',' '{ for( i=1; i<=NF; i++ ) print $i }' <<<"${STEPS}")
+  # check if steps are supported
+  not_supported_steps=""
+  for step in "${steps[@]}";
+  do
+    matched=false
+    for key in "${orders[@]}";
+    do
+      debug "execute cmd ${key}"
+      if [[ "${step}" == "${key}" ]]; then
+        matched=true;
+      fi
+    done
+    if [[ "$matched" == 'false' ]]; then
+      not_supported_steps="${not_supported_steps} $step"
+    fi
+  done
+  if [[ -z "${not_supported_steps}" ]]; then
+    for step in "${steps[@]}";
+    do
+      debug "execute cmd ${step}"
+      ${cmd_arr[${step}]}
+    done
+  else
+    err "following steps are not supported ${not_supported_steps}"
+    exit 1
+  fi
+fi
+
+set_github_action_outputs "${PR_BRANCH}" "${TEMPLATE_GIT_HASH}"
